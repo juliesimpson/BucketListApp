b6282bc51070ce98119d489dd7e9eb7675b02615
juliesimpson <julie.simpson326@gmail.com>
2017-02-20 13:41:25 -0500

    setting up server routes

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ var express = require("express");
 var http = require("http");
 var bodyParser = require("body-parser");
 var app = express();
+var router = require("./router");
 
+// Middleware
 app.use(bodyParser.json({ type: "*/*" }));
+router(app);
 
 var port = process.env.PORT || 3000;
 
-// creates a node server
+// creates a node server; createServer is built-in method
 // var server = http.createServer();   
 var server = http.createServer(app);
 
