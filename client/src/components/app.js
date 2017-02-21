b4a9b6433b7b3a597f88f709dd3a86f7f21b14be
juliesimpson <julie.simpson326@gmail.com>
2017-02-21 14:47:55 -0500

    props intro

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 
 import JSXIntro from "./1_JSXIntro";
-
 import ComponentsIntro from "./2_ComponentsIntro";
+import PropsIntro from "./3_PropsIntro";
+
 
 	export default class App extends Component {
 		render() {
@@ -11,6 +12,7 @@ import ComponentsIntro from "./2_ComponentsIntro";
 					<h1>React Intro</h1>
 					<JSXIntro />
 					<ComponentsIntro />
+					<PropsIntro name="Michael Bolton" title="The Neverending Elevator Music" />
 				</div>
 			);
 		}
