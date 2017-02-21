f2a83de678e6c79e55cd7d2a7de9b085eb371fe9
juliesimpson <julie.simpson326@gmail.com>
2017-02-21 16:13:11 -0500

    child components intro

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -4,6 +4,7 @@ import JSXIntro from "./1_JSXIntro";
 import ComponentsIntro from "./2_ComponentsIntro";
 import PropsIntro from "./3_PropsIntro";
 import StateIntro from "./4_StateIntro";
+import ChildComponentsIntro from "./5_ChildComponentsIntro";
 
 
 	export default class App extends Component {
@@ -15,6 +16,7 @@ import StateIntro from "./4_StateIntro";
 					<ComponentsIntro />
 					<PropsIntro name="Michael Bolton" title="The Neverending Elevator Music" />
 					<StateIntro />
+					<ChildComponentsIntro />
 				</div>
 			);
 		}
